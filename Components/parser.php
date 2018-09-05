1c201a7d2bd2cd31c61b7406aadbabe37c28d119
AlMA PRO LEADER <alma.pro.leader@gmail.com>
2018-09-05 19:58:34 +0200

    Indent fix.

--- Components/parser.php
+++ Components/parser.php
@@ -18,96 +18,96 @@
 	class Parser
 	{
 
-    /**
-     * Parse one line of log & return a log entry
-     *
-     * @param   string  $log  log data to parse
-     *
-     * @return  array
-     */
-    public static function line($log)
-    {
-      if($log  ==  '' || strpos($log, ' - ') === false) return array();
-      $parted   =  explode(' - ', $log);
-      $type     =  strtolower($parted[0]);
-      $time     =  $parted[1];
-      $message  =  rtrim(substr($log, strlen($type.' - '.$time.' - ')));
-      $entry    =  array(
-        'time'     =>  $time,
-        'message'  =>  $message
-      );
-      switch($type)
-      {
-        case '[x]':
-          $entry['type']  =  'Error';
-          break;
+	    /**
+	     * Parse one line of log & return a log entry
+	     *
+	     * @param   string  $log  log data to parse
+	     *
+	     * @return  array
+	     */
+	    public static function line($log)
+	    {
+			if($log  ==  '' || strpos($log, ' - ') === false) return array();
+			$parted   =  explode(' - ', $log);
+			$type     =  strtolower($parted[0]);
+			$time     =  $parted[1];
+			$message  =  rtrim(substr($log, strlen($type.' - '.$time.' - ')));
+			$entry    =  array(
+				'time'     =>  $time,
+				'message'  =>  $message
+			);
+			switch($type)
+			{
+			case '[x]':
+			  $entry['type']  =  'Error';
+			  break;
 
-        case '[+]':
-          $entry['type']  =  'Good';
-          break;
+			case '[+]':
+			  $entry['type']  =  'Good';
+			  break;
 
-        case '[*]':
-          $entry['type']  =  'Info';
-          break;
+			case '[*]':
+			  $entry['type']  =  'Info';
+			  break;
 
-        case '[!]':
-          $entry['type']  =  'Warning';
-          break;
+			case '[!]':
+			  $entry['type']  =  'Warning';
+			  break;
 
-        case '[**]':
-          $entry['type']  =  'Access';
-          break;
+			case '[**]':
+			  $entry['type']  =  'Access';
+			  break;
 
-        case '[!!]':
-          $entry['type']  =  'Hack';
-          break;
+			case '[!!]':
+			  $entry['type']  =  'Hack';
+			  break;
 
-        default:
-          $entry['type']  =  'Unknown';
-          break;
-      }
-      return $entry;
-    }
+			default:
+			  $entry['type']  =  'Unknown';
+			  break;
+			}
+			return $entry;
+	    }
 
 		/**
 		 * Parse a log file and return all log entries
-     *
-     * @param   string  $file  a log file to parse
+		 *
+		 * @param   string  $file  a log file to parse
 		 *
 		 * @return  array
 		 */
 		public static function parse($file)
 		{
-      $logs  =  array();
-      $sfo   =  new \SplFileObject($file, 'r');
-      $sfo->seek(1);
-      while($sfo->valid())
-      {
-        $log  =  static::line($sfo->current());
-        if(isset($log['type'])) array_push($logs, $log);
-        $sfo->seek($sfo->key() + 3);
-      }
-      return $logs;
+	    	$logs  =  array();
+			$sfo   =  new \SplFileObject($file, 'r');
+			$sfo->seek(1);
+			while($sfo->valid())
+			{
+				$log  = static::line($sfo->current());
+				if(isset($log['type'])) array_push($logs, $log);
+				$sfo->seek($sfo->key() + 1);
+			}
+			return $logs;
 		}
 
 		/**
 		 * Return only the last log entry of a log file
-     *
-     * @param   string  $file  a log file to get the last log entry from
+	     *
+	     * @param   string  $file  a log file to get the last log entry from
 		 *
 		 * @return  array
 		 */
 		public static function last($file)
 		{
 			$sfo  =  new \SplFileObject($file, 'r');
-      $sfo->seek(PHP_INT_MAX);
-      $log  =  '';
-      while(strpos($log, ' - ') === false && $sfo->key() > 0)
-      {
-        $sfo->seek($sfo->key() - 1);
-        $log  =  $sfo->current();
-      }
-      return static::line($log);
+			$sfo->seek(PHP_INT_MAX);
+			$log  =  '';
+			while(strpos($log, ' - ') === false && $sfo->key() > 0)
+			{
+				$sfo->seek($sfo->key() - 1);
+				$log  =  $sfo->current();
+			}
+			return static::line($log);
 		}
 	}
 ?>
