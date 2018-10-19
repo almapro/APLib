4ec91507413409ab731ca3fc45eb3dbba4e1b135
AlMA PRO LEADER <alma.pro.leader@gmail.com>
2018-10-20 01:11:27 +0200

    Removed tests from WebSockets\Message.

--- Components/WebSockets/message.php
+++ Components/WebSockets/message.php
@@ -11,25 +11,21 @@
 
         public function onOpen($conn)
         {
-            echo "Open\r\n";
             \APLib\WebSockets::add($conn);
         }
 
         public function onMessage($from, $msg)
         {
-            echo "Message: {$msg}\r\n";
             \APLib\WebSockets::respond($from, $msg);
         }
 
         public function onClose($conn)
         {
-            echo "Close\r\n";
             \APLib\WebSockets::remove($conn);
         }
 
         public function onError($conn, $e)
         {
-            echo "Error\r\n";
             \APLib\Logger::Error($e);
             $conn->close();
         }
